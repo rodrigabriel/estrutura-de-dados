fd84f69d9ac23b60d220de96e1a178cc8df7cbc3
Gabriel <gabriel17ro@gmail.com>
2023-05-25 13:29:28 -0400

    Checkpoint

---
+++ main.py
@@ -0,0 +1,27 @@
+# estrutura de dados 2 - árvore binária
+# aluno: Gabriel Rodrigues Sousa Silva
+
+
+class ArvoreBinaria:
+
+    def __init__(self):
+        self.raiz = None
+        self.esquerdo = None
+        self.direito = None
+        self.altura = None
+        self.nivel = None
+    
+
+    def inicializa(self):
+        pass
+
+
+    def adicionar(self):
+        pass
+
+
+    def remover(self):
+        pass
+
+
+    
