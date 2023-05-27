cec5408797d35f0bc237a553bd1be8947ec37df2
Gabriel <gabriel17ro@gmail.com>
2023-05-27 18:49:35 -0400

    Checkpoint: método de adicionar pronto

--- main.py
+++ main.py
@@ -2,55 +2,65 @@
 # aluno: Gabriel Rodrigues Sousa Silva
 
 
+class No: # Necessário para não ter congestionamento/conflito entre os nós.
+
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerdo = None
+        self.direito = None
+
+
 class ArvoreBinaria:
 
     def __init__(self):
         self.raiz = None
-        self.esquerdo = None
-        self.direito = None
         self.altura = None
         self.nivel = None
-    
-
-#    def inicializa(self, valor):
-#        pass
-
 
     def adicionar(self, valor):
         if self.raiz == None:
-            self.raiz = valor
+            self.raiz = No(valor)
         else:
-            self._verificacaoRecursiva(valor, self.raiz)
+            self._recursaoDeInsercao(valor, self.raiz)
+
+
+    def _recursaoDeInsercao(self, valor, noAtual): # Método auxiliar
+        if valor < noAtual.valor: # noAtual.valor -> acessa o valor do momento, que foi inserido no método principal de adicionar
+            if noAtual.esquerdo is None:
+                noAtual.esquerdo = No(valor)
+            else:
+                self._recursaoDeInsercao(valor, noAtual.esquerdo)
+        else:
+            if noAtual.direito is None:
+                noAtual.direito = No(valor)
+            else:
+                self._recursaoDeInsercao(valor, noAtual.direito)
+            
+
+    def _mUDARNOME():
+        pass
+
+    def preOrdem(self):
+        pass
+
+    def remover(self, valor):
         pass
 
 
-    def _verificacaoRecursiva(self, valor, noAtual):
-        if valor < noAtual:
-            if self.esquerdo == None:
-                self.esquerdo = valor
-            else:
-                self._verificacaoRecursiva(valor, self.esquerdo)
-        else:
-            if self.direito == None:
-                self.direito = valor
-            else:
-                self._verificacaoRecursiva(valor, self.direito)
-
-
-    def remover(self):
-        pass
-
-
-    def imprimeraiz(self):
-        print(self.raiz)
+arvore = ArvoreBinaria()
+arvore.adicionar(5)
+arvore.adicionar(8)
+arvore.adicionar(1)
+arvore.adicionar(5)
+arvore.adicionar(6)
 
 
 
-no = ArvoreBinaria()
 
-no.adicionar(5)
-no.adicionar(7)
-no.adicionar(2)
-no.imprimeraiz()
-print(no.direito)
-print(no.esquerdo)
+"""
+# ANOTAÇÕES:
+Ajustando a inserção de novos nós.
+
+TO-DO:
+Organizar a consulta para exibir os itens da árvore.
+"""
